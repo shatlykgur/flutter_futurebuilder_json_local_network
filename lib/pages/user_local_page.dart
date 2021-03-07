2f327dcf7e3cfb57aec74c476f7acec9876d610c
Shatlyk Gurbandurdyyev <shatlykgur@gmail.com>
2021-03-07 14:04:24 +0300

    json data

---
+++ lib/pages/user_local_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UserLocalPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<List<User>>(
+
+      ),
+    );
+  }
+}
